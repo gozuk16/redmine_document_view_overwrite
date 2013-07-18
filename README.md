7b5afd33a4d89ae530ae4709dcaec59fde6bab68
GOZU Kenichiro <k.gozu@infocom.co.jp>
2013-07-18 10:30:25 +0900

    initial commit.

---
+++ README.md
@@ -0,0 +1,14 @@
+# Redmine document view overwrite plugin
+- 文書の一覧表示を変更するプラグインです。
+    - 文書の並び順を降順にします。
+    - 1文書あたりの表示が長すぎると感じるので、表示を切り詰めました。
+
+## Instllation
+- モジュールのコピー
+```
+$ git clone git://github.com/gozuk16/redmine_document_view_overwrite.git plugins/redmine_document_view_overwrite
+```
+- redmineの再起動
+
+## Versions
+- redmine 2.2.x
---
+++ init.rb
@@ -0,0 +1,10 @@
+# encoding: utf-8
+
+Redmine::Plugin.register :redmine_document_view_overwrite do
+  name 'Redmine Document View Overwrite plugin'
+  author 'GOZU Kenichiro'
+  description '文書の並び順を日付の降順にし、1文書あたり長すぎるので表示を切り詰めました。'
+  version '0.0.1'
+  url 'https://github.com/gozuk16/redmine_document_view_overwrite'
+  author_url 'https://github.com/gozuk16'
+end
